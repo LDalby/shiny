5cd5021c437ef56988f68c46b0d0b5fda0df1cb1
Lars Dalby <lars.dalby@gmail.com>
2016-10-19 14:25:53 +0200

    fix names bug

--- Starlings/app.R
+++ Starlings/app.R
@@ -29,7 +29,7 @@ server <- function(input, output, session) {
   getSpData<-reactive({
     # Return a subset based on user input:
     tmp = fields[, input$fieldseason]
-    names(tmp)
+    names(tmp) = "values"
     tmp
   })
   # Incremental changes to the map:
