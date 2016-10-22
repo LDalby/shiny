916306a716b950737de4d6e0107b8f132d3b0e7f
Lars Dalby <lars.dalby@gmail.com>
2016-10-22 11:21:18 +0200

    Add css style file and apply.

--- Starlings/app.R
+++ Starlings/app.R
@@ -17,8 +17,11 @@ vars <- c(
 ui <- navbarPage("Starlings", id = "nav",
  tabPanel("Map", 
    div(class="outer",
-      tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
-      leafletOutput("hjortkaerMap"),
+      tags$head(
+        # Test if this will cure the full screen issue
+        includeCSS("style.css")
+        ),
+      leafletOutput("hjortkaerMap", height = "100%", width = "100%"),
       absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
        draggable = TRUE, top = 60, left = "auto", right = 20, bottom = "auto",
        width = 330, height = "auto",
